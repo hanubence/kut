06c21597df9569ccae2466810e9f17e713e56911
hanubence <hanuszkabence@gmail.com>
2021-04-13 08:59:30 +0200

    updated ui

--- js/index.js
+++ js/index.js
@@ -1,5 +1,14 @@
-const dropdown = document.querySelector(".navbar-dropdown");
-const dropdownContent = document.querySelector(".dropdown-content");
-dropdown.addEventListener("click", () => {
-	dropdownContent.classList.toggle("open");
+let buttons = document.querySelectorAll(".navbar-item");
+
+buttons.forEach((el) => {
+	el.addEventListener("click", (e) => {
+		buttons.forEach((el) => {
+			el.classList.remove("selected");
+		});
+		if (e.target.nodeName == "LI") {
+			e.target.classList.add("selected");
+		} else {
+			e.target.parentElement.classList.add("selected");
+		}
+	});
 });
