f90a3b49201fff5ee8593b460074a5c53f2e0a7b
hanubence <hanuszkabence@gmail.com>
2021-07-09 18:52:07 +0200

    navbar changes

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,18 @@
 function toggleNav(event) {
     let nav = document.querySelector(".mobile-nav");
-    //nav.classList.toggle("hidden");
-    //nav.classList.toggle("invisible");
+    let burger = document.querySelector(".hamburger");
+    let kereszt = document.querySelector(".kereszt");
+
     nav.classList.toggle("left-1/4");
     nav.classList.toggle("left-full");
+
+    burger.classList.toggle("visible");
+    burger.classList.toggle("block");
+    burger.classList.toggle("invisible");
+    burger.classList.toggle("hidden");
+
+    kereszt.classList.toggle("visible");
+    kereszt.classList.toggle("block");
+    kereszt.classList.toggle("invisible");
+    kereszt.classList.toggle("hidden");
 } 
