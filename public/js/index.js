54d968a62237210cdef845d2c4d2e90e8ed6bdd0
hanubence <hanuszkabence@gmail.com>
2021-07-13 02:21:33 +0200

    optimization changes

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,18 @@
 function toggleNav(event) {
-    let nav = document.querySelector(".mobile-nav");
-    let burger = document.querySelector(".hamburger");
-    let kereszt = document.querySelector(".kereszt");
+	let nav = document.querySelector(".mobile-nav");
+	let burger = document.querySelector(".hamburger");
+	let kereszt = document.querySelector(".kereszt");
 
-    nav.classList.toggle("left-1/4");
-    nav.classList.toggle("left-full");
+	nav.classList.toggle("left-1/4");
+	nav.classList.toggle("left-full");
 
-    burger.classList.toggle("visible");
-    burger.classList.toggle("block");
-    burger.classList.toggle("invisible");
-    burger.classList.toggle("hidden");
+	burger.classList.toggle("visible");
+	burger.classList.toggle("block");
+	burger.classList.toggle("invisible");
+	burger.classList.toggle("hidden");
 
-    kereszt.classList.toggle("visible");
-    kereszt.classList.toggle("block");
-    kereszt.classList.toggle("invisible");
-    kereszt.classList.toggle("hidden");
-} 
+	kereszt.classList.toggle("visible");
+	kereszt.classList.toggle("block");
+	kereszt.classList.toggle("invisible");
+	kereszt.classList.toggle("hidden");
+}
